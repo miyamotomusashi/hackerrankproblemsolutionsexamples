255e379c4f894b87269ce8d2e25c7ed5d233314e
Saadettin Ördekçi <cenbek03@hotmail.com>
2023-06-17 23:48:29 +0300

    Add project files.

---
+++ DiagonalDifference.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions
+{
+  public class DiagonalDifference
+  {
+    List<List<int>> matrice = new List<List<int>>();
+    public DiagonalDifference() {
+      matrice.Add(new List<int>() { 6, 2, 3 });
+      matrice.Add(new List<int>() { 1, 8, 2 });
+      matrice.Add(new List<int>() { 3, 4, 7 });
+    }
+
+    public void WriteDiagonalMatrice()
+    { 
+      for(int i=0;i<matrice.Count();i++)
+      {
+        for (int j = 0; j < matrice[i].Count(); j++)
+        {
+          Console.Write(matrice[i][j] + " ");
+        }
+        Console.WriteLine();
+      }
+    }
+
+    public int CalculateDiagonalDifference()
+    {
+      int right = 0;
+      int left = 0;
+      int result = 0;
+
+      for (int i = 0; i < matrice.Count; i++)
+      {
+        for (int j = 0; j < matrice.Count; j++)
+        {
+          right = matrice[0][0] + matrice[1][1] + matrice[2][2];
+          left = matrice[2][0] + matrice[1][1] + matrice[0][2];
+        }
+
+        result = Math.Abs(right - left);
+      }
+
+      return result;
+    }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace HackerRankSolutions
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+
+      DiagonalDifference matriceDifference = new DiagonalDifference();
+      matriceDifference.WriteDiagonalMatrice();
+      Console.WriteLine(matriceDifference.CalculateDiagonalDifference());
+
+
+    }
+  }
+}
