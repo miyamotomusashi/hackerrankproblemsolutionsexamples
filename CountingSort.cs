ad5cfb1ef0ba5abed680a145f20877e51c3f4314
Saadettin Ördekçi <cenbek03@hotmail.com>
2023-06-19 00:40:31 +0300

    counting sort

---
+++ CountingSort.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions
+{
+  
+  public class CountingSort
+  {
+    public List<int> _numberArray { get; set; } = new List<int>();
+    public CountingSort() { }
+
+
+    public int GetMaxVal(List<int> array, int size)
+    {
+      var maxVal = array[0];
+      for (int i = 1; i < size; i++)
+        if (array[i] > maxVal)
+          maxVal = array[i];
+      return maxVal;
+   }
+
+    public void CountingSortMethod(List<int> array)
+    {
+      var size = array.Count();
+      var maxElement = GetMaxVal(array, size);
+      var occurrences = new int[maxElement + 1];
+      for (int i = 0; i < maxElement + 1; i++)
+      {
+        occurrences[i] = 0;
+      }
+      for (int i = 0; i < size; i++)
+      {
+        occurrences[array[i]]++;
+      }
+      for (int i = 0, j = 0; i <= maxElement; i++)
+      {
+        while (occurrences[i] > 0)
+        {
+          array[j] = i;
+          j++;
+          occurrences[i]--;
+        }
+      }
+
+      foreach (int i in array)
+      {
+        Console.Write(i + ",");
+      }
+      //return array;
+    }
+
+  }
+
+
+}
--- Program.cs
+++ Program.cs
@@ -9,10 +9,19 @@ namespace HackerRankSolutions
   {
     static void Main(string[] args)
     {
+      //DIAGONAL DIFFERENCE
+      //DiagonalDifference matriceDifference = new DiagonalDifference();
+      //matriceDifference.WriteDiagonalMatrice();
+      //Console.WriteLine(matriceDifference.CalculateDiagonalDifference());
 
-      DiagonalDifference matriceDifference = new DiagonalDifference();
-      matriceDifference.WriteDiagonalMatrice();
-      Console.WriteLine(matriceDifference.CalculateDiagonalDifference());
+      //COUNTING SORT
+      CountingSort sort = new CountingSort();
+      //var array = new int[] { 73, 57, 49, 99, 133, 20, 1 };
+      List<int> listArray = new List<int>() { 73, 57, 49, 99, 133, 20, 1 }; 
+      sort._numberArray = listArray;
+      sort.CountingSortMethod(sort._numberArray);
+
+
 
 
     }
